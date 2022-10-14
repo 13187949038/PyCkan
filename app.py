e68a02010fb25645c9128e25d7fc4c4dfaf1f512
XYCode <ysydcbj169@wep.email>
2022-10-14 19:56:37 +0800

    新增：元数据的获取

--- app.py
+++ app.py
@@ -1,4 +1,7 @@
 import parser.meta
 
 
-parser.meta.get_metadata()
+# metadata_dirs = [parser.meta.get_metadata()]
+
+# for dir in metadata_dirs:
+parser.meta.analyse_metadata('/tmp/tmpcpigmiydpyckan_metadata/default')
